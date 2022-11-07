7c474246f013a0103b511c3bb61b0fd804f91d4d
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-11-06 22:41:51 -0300

    Modelo Inicial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('Linkado')
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => document.getElementById('modal')
+    .classList.remove('active')
+
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)

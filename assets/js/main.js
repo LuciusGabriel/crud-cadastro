38c7dae1b77a22fd42c2c93685973590cc74d707
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-11-08 23:01:28 -0300

    function Create

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,32 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+
+const tempClient = {
+    nome: "Lucius",
+    email: "lucius_gabriel@hotmail.com",
+    celular: "62991852208",
+    cidade: "Anápolis"
+
+}
+
+const getLocalStorage = ()=> JSON.parse(localStorage.getItem('db_client')) ?? [] //JSON.parse -> Transforma o string de volta em um Objeto JSON 
+//localStorage.getItem Irá ler o que está dentro de db_client e depois adicionar um novo.
+// ?? compara como um IF se não for válido cria um array vazio
+const setLocalStorage = (dbClient)=> localStorage.setItem("db_client", //O metodo .setItem envia o dado para o LocalStorage, recebe dois parametros (chave/Key, valor/Value)
+JSON.stringify(dbClient)) //O método JSON.stringify transforma um Objeto em String
+
+
+
+//CREATE - CRIAR O LOCALSTORAGE PARA ARMAZENAR OS DADOS
+const createClient = (client)=>{
+    const dbClient = getLocalStorage()
+    dbClient.push(client) //Método .push empurra os dados para dentro
+    setLocalStorage(dbClient)
+}
+
+
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
